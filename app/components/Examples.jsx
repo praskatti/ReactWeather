1ed28172cdaf342bdf577d00374b00859f390ce1
Katti, Mahantesh Prasad <mkatti@visa.com>
2017-06-10 20:01:20 +0530

    Added

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -14,7 +14,10 @@ var React = require('react');
 var Examples = (props) =>{
 
     return (
+      <div>
         <h3>Examples Component !!</h3>
+        <p>Welcine to Examples Page </p>
+      </div>
      );
 }
 module.exports = Examples;
