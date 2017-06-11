556f6bef41c91f68511b2a06eb96cf32c01853f6
Katti, Mahantesh Prasad <mkatti@visa.com>
2017-06-11 11:25:51 +0530

    about.js

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -14,8 +14,10 @@ var React = require('react');
 var About = (props) => {
     debugger;
     return(
+      <div>
       <h3>About </h3>
       <p>Welcome to About Page !!</p>
+    </div>
     );
 };
 
