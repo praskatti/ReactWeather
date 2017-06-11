6a2bdc20de025e7814121d6c23d9e23c7cd22888
Katti, Mahantesh Prasad <mkatti@visa.com>
2017-06-11 13:39:21 +0530

    addfiundationjquery

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,9 @@ var Weather = require('./components/Weather');
 var About = require('./components/About');
 var Examples = require('./components/Examples');
 
+//Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
  ReactDOM.render(
   <Router history={hashHistory}>
       <Route path="/"  component={Main} >
